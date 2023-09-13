d292be849aa7b12a5fbca172cca24132c04fbc0a
VladyslavBohutskyi <nonseinvenit@gmail.com>
2023-09-13 14:28:29 +0800

    fix-header

--- dev/src/header.js
+++ dev/src/header.js
@@ -5,9 +5,25 @@ if (!customElements.get('s-header')) {
     }
 
     connectedCallback() {
+      this.mobMenu()
+      this.stickyHeader()
+    }
+
+
+    mobMenu() {
       this.querySelector('.s-header__menu-btn').addEventListener('click',
         () => (this.classList.toggle('s-header--menu-active'),
           document.querySelector('body').classList.toggle('hiden')))
+    }
+    
+    stickyHeader(){
+      console.log(this.getAttribute('data-sticky'));
+      function addSticky(){
+        if(document.documentElement.scrollTop > 200){
+          this.classList.add('sticky')
+        }
+      }
+      window.addEventListener('scroll', (e) => console.log(document.documentElement.scrollTop))
     }
 
   });
