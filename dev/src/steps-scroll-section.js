d3989d08f0c59b0e78408d88ca2baad8c6a6cd8b
VladyslavBohutskyi <nonseinvenit@gmail.com>
2023-09-27 16:58:15 +0800

    fix section

--- dev/src/steps-scroll-section.js
+++ dev/src/steps-scroll-section.js
@@ -1,42 +1,85 @@
-if(!customElements.get('s-steps-scroll')){
+if (!customElements.get('s-steps-scroll')) {
 
-  customElements.define('s-steps-scroll', class SStepsScroll extends HTMLElement{
-  constructor(){
-    super();
-  }
+  customElements.define('s-steps-scroll', class SStepsScroll extends HTMLElement {
+    constructor() {
+      super();
+    }
 
-  connectedCallback(){
-   this.scrollEvent()
-  }
-  activeImages = [1,2]
-  checkHeight(){
+    connectedCallback() {
+      this.scrollEvent()
+      this.querySelector('.s-steps-scroll').style.height = this.sectionHeight + 'px'
+      this.querySelector('.s-steps-scroll__row').style.height = '700px'
+    }
 
-    const images = this.querySelectorAll('.s-steps-scroll__images__image')
-    const contents = Array.from(this.querySelectorAll('.s-steps-scroll__content__step'))
+    activeImages = []
+    imagesTranslate = 0
+    sectionHeight = this.querySelector('.s-steps-scroll__row').offsetHeight
+    srollPosImage = 0
+    sectionTopValue = 0
+    sectionBottomValue = 0
 
-    images.forEach((e) => {
-      if(e.getBoundingClientRect().y < (Math.round(window.screen.availHeight/3))){
-        if(!this.activeImages.includes(e.getAttribute('data-image-index'))){
-          contents[e.getAttribute('data-image-index')-2] == undefined ? false : contents[e.getAttribute('data-image-index')-2].classList = 's-steps-scroll__content__step s-steps-scroll__content__step--top'
-          contents[e.getAttribute('data-image-index')-1] == undefined ? false : contents[e.getAttribute('data-image-index')-1].classList = 's-steps-scroll__content__step s-steps-scroll__content__step--active'
-          contents[e.getAttribute('data-image-index')] == undefined ? false : contents[e.getAttribute('data-image-index')].classList = 's-steps-scroll__content__step s-steps-scroll__content__step--bottom'
-          this.activeImages.push(e.getAttribute('data-image-index'));
-        } 
+    toggleFixedSection() {
+      if (this.querySelector('.s-steps-scroll__row').getBoundingClientRect().y < 100 && this.sectionTopValue == 0) {
+        this.querySelector('.s-steps-scroll__row').classList.add('s-steps-scroll__row--active')
+        this.querySelector('.s-steps-scroll__row').style.transform = 'translateY(0px)'
+        this.sectionTopValue = window.scrollY
+      } else if (window.scrollY < this.sectionTopValue) {
+        this.querySelector('.s-steps-scroll__row').classList.remove('s-steps-scroll__row--active')
+        this.sectionTopValue = 0
+        console.log('2 yclovie');
+      } else if (window.scrollY > (this.sectionTopValue + this.sectionHeight - 500)) {
+        this.querySelector('.s-steps-scroll__row').classList.remove('s-steps-scroll__row--active')
+        this.querySelector('.s-steps-scroll__row').style.transform = 'translateY(' + (this.sectionHeight - 600) + 'px)'
+        console.log('3 yclovie');
+        this.sectionBottomValue = window.scrollY
+      } else if (window.scrollY < (this.sectionTopValue + this.sectionHeight - 500) && this.sectionBottomValue > 0) {
+        this.sectionBottomValue = 0
+        this.querySelector('.s-steps-scroll__row').classList.add('s-steps-scroll__row--active')
+        this.querySelector('.s-steps-scroll__row').style.transform = 'translateY(0px)'
       }
-      else if (e.getBoundingClientRect().y > (Math.round(window.screen.availHeight)/2) && this.activeImages.includes(e.getAttribute('data-image-index'))){
-        contents[e.getAttribute('data-image-index')-3] == undefined ? false : contents[e.getAttribute('data-image-index')-3].classList = 's-steps-scroll__content__step s-steps-scroll__content__step--top'
-        contents[e.getAttribute('data-image-index')-2] == undefined ? false : contents[e.getAttribute('data-image-index')-2].classList = 's-steps-scroll__content__step s-steps-scroll__content__step--active'
-        contents[e.getAttribute('data-image-index')-1] == undefined ? false : contents[e.getAttribute('data-image-index')-1].classList = 's-steps-scroll__content__step s-steps-scroll__content__step--bottom'
-        this.activeImages.pop();
+    }
+
+    translateImages() {
+      const wrapperImages = this.querySelector('.s-steps-scroll__images')
+      if (this.sectionTopValue != 0) {
+        wrapperImages.style.transform = 'translateY(' + (this.sectionTopValue - window.scrollY) + 'px)'
       }
-    })
-  }
 
-  scrollEvent() {
-    window.addEventListener('scroll', () => this.checkHeight())
-  }
-  
+      console.log(window.scrollY);
+      console.log(this.sectionTopValue + this.sectionHeight);
 
-})
+      // e.querySelector('img').style.top = -(window.scrollY - this.srollPosImage)/10 + 'px' -------scroll
+      // this.srollPosImage = window.scrollY   -------scroll
+
+    }
+
+    switchSteps() {
+
+      const images = this.querySelectorAll('.s-steps-scroll__images__image')
+      const contents = Array.from(this.querySelectorAll('.s-steps-scroll__content__step'))
+      images.forEach((e) => {
+        if (e.getBoundingClientRect().y < (Math.round(window.screen.availHeight / 3))) {
+          if (!this.activeImages.includes(e.getAttribute('data-image-index'))) {
+            contents[e.getAttribute('data-image-index') - 2] == undefined ? false : contents[e.getAttribute('data-image-index') - 2].classList = 's-steps-scroll__content__step s-steps-scroll__content__step--top'
+            contents[e.getAttribute('data-image-index') - 1] == undefined ? false : contents[e.getAttribute('data-image-index') - 1].classList = 's-steps-scroll__content__step s-steps-scroll__content__step--active'
+            contents[e.getAttribute('data-image-index')] == undefined ? false : contents[e.getAttribute('data-image-index')].classList = 's-steps-scroll__content__step s-steps-scroll__content__step--bottom'
+            this.activeImages.push(e.getAttribute('data-image-index'));
+          }
+        }
+        else if (e.getBoundingClientRect().y > (Math.round(window.screen.availHeight) / 2) && this.activeImages.includes(e.getAttribute('data-image-index'))) {
+          contents[e.getAttribute('data-image-index') - 3] == undefined ? false : contents[e.getAttribute('data-image-index') - 3].classList = 's-steps-scroll__content__step s-steps-scroll__content__step--top'
+          contents[e.getAttribute('data-image-index') - 2] == undefined ? false : contents[e.getAttribute('data-image-index') - 2].classList = 's-steps-scroll__content__step s-steps-scroll__content__step--active'
+          contents[e.getAttribute('data-image-index') - 1] == undefined ? false : contents[e.getAttribute('data-image-index') - 1].classList = 's-steps-scroll__content__step s-steps-scroll__content__step--bottom'
+          this.activeImages.pop();
+        }
+      })
+    }
+
+    scrollEvent() {
+      window.addEventListener('scroll', () => (this.switchSteps(), this.toggleFixedSection(), this.translateImages()))
+    }
+
+
+  })
 
 }
